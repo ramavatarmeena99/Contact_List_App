200a4c22d36c123baaef07e643a77293eabe74a7
ramavatarmeena99 <meenaramavatar48@gmail.com>
2022-10-13 20:15:07 +0530

    persist add

--- src/Redux/store.js
+++ src/Redux/store.js
@@ -1,8 +1,23 @@
 import { combineReducers, createStore } from "redux";
+import { persistStore, persistReducer } from "redux-persist";
 import countactReducer from "./reducers";
+import storage from "redux-persist/lib/storage"; // defaults to localStorage for web
+
+const persistConfig = {
+  key: "root",
+  storage,
+  whitelist: ["countactReducer"],
+}; 
 
 const rootReducer = combineReducers({
   countactReducer,
 });
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
-export const store = createStore(rootReducer);
+
+export const store = createStore(persistedReducer);
+
+export const persistor = persistStore(store);
+
+
+
--- src/index.js
+++ src/index.js
@@ -4,13 +4,17 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { Provider } from "react-redux";
-import { store } from "./Redux/store";
+import { PersistGate } from "redux-persist/integration/react"
+import { store, persistor } from "./Redux/store";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
+
       <App />
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
